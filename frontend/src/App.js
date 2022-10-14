66d9c896ce2f34458db82c9219b656183bafc770
Pradyut123 <63655969+Pradyut123@users.noreply.github.com>
2022-10-14 15:56:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { Container } from "react-bootstrap";
 import Jobs from "./Jobs";
-import SearchBar from "./SearchBar";
+
 
 function App() {
   return (
     <Container className="my-4">
       <h1 className="mb-4">Ads Search Website</h1>
-      {/* <SearchBar/> */}
+      
       <Jobs />
     </Container>
   );
